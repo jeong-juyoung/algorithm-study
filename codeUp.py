dd50c8097707f7b0132f7d638c47950203e1f365
jeong-juyoung <jungjuyoung1314@gmail.com>
2022-05-30 14:56:43 +0900

    문자열 재정렬

--- codeUp.py
+++ codeUp.py
@@ -85,4 +85,31 @@ for step in steps:
 print(result)
 
 
-#문자열 재정렬ㅇ
+#문자열 재정렬
+#요구사항대로 충실히 구현하면 되는 문제
+#문자열이 입력되었을 떄 문자를 하나씩 확인
+#숫자인 경우 따로 합계를 계산
+#알파벳은 경우 별도의 리스트에 저장
+#결과적으로 리스트에 저장된 알파벳을 정렬해 출력하고, 합계를 뒤에 붙여 출력하면 정답
+
+data = input()
+result = []
+value = 0 
+
+#문자를 하나씩 확인하며
+for x in data:
+    #알파벳인 경우 결과 리스트에 삽입
+    if x.isalpha():
+        result.append(x)
+    #숫자는 따로 더하기
+    else:
+        value += int(x)
+#알파벳을 오름차순으로 정렬
+result.sort()
+
+#숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+if value != 0:
+    result.append(str(value))
+
+#최종 결과 출력(리스트를 문자열로 변환하여 출력)
+print('' .join(result))
