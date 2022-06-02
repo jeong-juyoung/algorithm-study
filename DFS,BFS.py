e6e4677b134d08fd344d66e9c0c31a731fe289d9
jeong-juyoung <jungjuyoung1314@gmail.com>
2022-06-02 16:18:51 +0900

    BFS 예제 추가

--- DFS,BFS.py
+++ DFS,BFS.py
@@ -32,3 +32,72 @@ print('재귀적으로 구현:', factorial_recursive(5))
 
 
 #DFS 소스코드 예제
+#DFS 매서드 정의
+def dfs(graph, v, visited):
+    #현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    #현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+# 각 노드가 연결된 정보를 표현 (2차원 리스트)
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+#각 노드가 방문된 정보를 표현(1차원 리스트)
+visited = [False] * 9
+
+#정의된 DFS 함수 호출
+dfs(graph,1,visited)
+
+#BFS
+#BFS 소스코드 예제 
+
+from collections import deque
+
+
+#BFS 매서드 정의
+def bfs(graph,start,visited):
+    #큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    #현재 노드를 방문 처리
+    visited[start] = True
+    #큐가 빌 때까지 반복
+    while queue:
+    #큐에서 하나의 원소를 뽑아 출력하기
+         v = queue.popleft()
+         print(v, end=' ')
+         #아직 방문하지 않은 인접한 원소들을 큐에 삽입
+         for i in graph[v]:
+             if not visited[i]:
+                 queue.append(i)
+                 visited[i] = True
+
+#각 노드가 연결된 정보를 표현(2차원리스트)
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+#각 노드가 방문된 정보를 표현 (1차원 리스트)
+visited = [False] * 9 
+
+#정의된 BFS 함수 호출
+bfs(graph,1,visited)
