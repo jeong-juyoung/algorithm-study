e7bf91b52f740039e2e222544b3397071f933e8a
jeong-juyoung <jungjuyoung1314@gmail.com>
2022-06-21 13:20:43 +0900

    python 기초문법

---
+++ pythonTest.py
@@ -0,0 +1,20 @@
+#연습문제 제곱
+data = int(input())
+print(data * data)
+
+
+#while문 연습문제
+num = int(input())
+
+int = 0
+while int <= num:
+    print(num)
+    int += 1
+
+#while문 제곱
+num = int(input())
+
+int = 1
+while int <= num:
+    print(int, int*int)
+    int += 1
