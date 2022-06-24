4ad882df0979a7c4f7e0d10c17e007742217c51a
jeong-juyoung <jungjuyoung1314@gmail.com>
2022-06-24 10:00:24 +0900

    elif 기초

--- pythonTest.py
+++ pythonTest.py
@@ -18,3 +18,26 @@ int = 1
 while int <= num:
     print(int, int*int)
     int += 1
+
+
+# if-elif-else 연습문제
+
+num = int(input())
+result = str(num)
+
+if num >= 1000:
+    result = str(num // 1000) + 'k'
+elif num >= 0:
+    pass
+
+print(result)
+
+num = int(input())
+result = str(num)
+
+if num >= 1000000:
+    result = str(num // 1000000) + 'M'
+elif num >= 0:
+    pass
+
+print(result)
