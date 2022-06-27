e22ef9d575cb9628c8371b712f3ef0a4db6c8652
jeong-juyoung <jungjuyoung1314@gmail.com>
2022-06-27 16:05:24 +0900

    for문

--- pythonTest.py
+++ pythonTest.py
@@ -1,3 +1,4 @@
+
 #연습문제 제곱
 data = int(input())
 print(data * data)
@@ -41,3 +42,34 @@ elif num >= 0:
     pass
 
 print(result)
+
+# 양수만 덧셈하기
+sum = 0
+while True:
+    num = int(input())
+    if num < 0:
+        break
+    else:
+        sum += num
+
+print(sum)
+
+
+# 윤년 판별하기
+year = int(input())
+
+if year%4 != 0:
+    print("평년")
+elif year%100 != 0:
+    print("윤년")
+elif year%400 != 0:
+    print("평년")
+else: 
+    print("윤년")
+
+#for문
+
+famliy = ['mom' , 'father', 'jsy', 'jhy','me']
+
+for x in famliy:
+    print(x , len(x))  
